fda42cda067cfb5f5ea418a43573387f86c46408
nglshman <MittelfurFahigkeiten@gmail.com>
2019-04-04 00:31:11 +0300

    HTML header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
-
+  <div>
+    <header>
+      <h2>Orphan</h2>
+      <ul>
+        <li>Home</li>
+        <li>Who we are</li>
+        <li>Our mission</li>
+      </ul>
+      <ul>
+        <li>Page</li>
+        <li>Single post</li>
+        <li>Contact</li>
+      </ul>
+      <button>Join Us</button>
+    </header>
+</div>
 </template>
 
 <script>
